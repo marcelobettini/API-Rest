8cef22ce6e9964f5948da625d3ac851dc09ca979
unknown <marcelobettini@yahoo.com.ar>
2022-02-23 13:39:59 -0300

    dotenv reqired properly in db.js and .env file checked

--- db.js
+++ db.js
@@ -1,21 +1,23 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
 
 //MacBook
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  database: "api",
-  user: "root",
-  password: "root",
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-});
-
-//Windows PC
 // const connection = mysql.createConnection({
 //   host: "localhost",
+//   port: 8889,
 //   database: "api",
 //   user: "root",
-//   password: "",
+//   password: "root",
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
 // });
 
+//Windows PC
+console.log(process.env.DB_HOST);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+});
 module.exports = connection;
