993d5210bee0689a2b6b7d36b9c86a6c16d688cb
Marcelo Bettini <bettinimarcelo@gmail.com>
2022-03-26 21:11:30 -0300

    from connection to pool and all posts req ok

--- db/config.js
+++ db/config.js
@@ -1,24 +1,38 @@
+const mysql = require("mysql");
+const util = require("util");
 //MacBook
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   database: "api",
-//   user: "root",
-//   password: "root",
-//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-// });
-const mysql = require("mysql")
-const util = require("util")
-
-const connection = mysql.createConnection({
-    host: process.env.db_host,
-    database: process.env.db_name,
-    user: process.env.db_user,
-    pass: process.env.db_pass
+/* MAMP config for MacBookPro M1 */
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS_MAC,
+  socketPath: process.env.DB_SOCKET,
+  port: process.env.DB_PORT,
 });
-connection.connect((err) => {
-    err ? console.warn("No conectado", { "error": err.message }) : console.log("Conexión establecida...")
-})
-connection.query = util.promisify(connection.query)
 
-module.exports = connection
+//check connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.warn("No conectado", { error: err.message });
+  } else {
+    console.log("Connection to DB Ok!");
+    connection.release();
+    return;
+  }
+});
+
+// const connection = mysql.createConnection({
+// host: process.env.db_host,
+// database: process.env.db_name,
+// user: process.env.db_user,
+// pass: process.env.db_pass
+// });
+// connection.connect((err) => {
+//   err
+//     ? console.warn("No conectado", { error: err.message })
+//     : console.log("Conexión establecida...");
+// });
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
