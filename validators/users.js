ac90fbc148a590ea14c026c29219ba0383ec040e
unknown <marcelobettini@yahoo.com.ar>
2022-03-29 16:54:08 -0300

    sanitization with trim added

--- validators/users.js
+++ validators/users.js
@@ -1,15 +1,15 @@
 const { check, validationResult } = require("express-validator");
 
 const validatorCreateUser = [
-    check("name")
-    .exists()
-    .notEmpty()
-    .isLength({ min: 2, max: 30 }),
+    check("name").trim()
+    .exists().withMessage("Name field is required")
+    .notEmpty().withMessage("Must not be empty")
+    .isLength({ min: 2, max: 30 }).withMessage("Character count: 2 min, 30 max"),
     check("email")
     .exists()
     .notEmpty()
     .isEmail(),
-    check("password")
+    check("password").trim()
     .exists()
     .notEmpty()
     .isLength({ min: 6, max: 30 }),
