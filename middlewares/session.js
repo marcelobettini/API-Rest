ff919ae3dc4807782a0b0971430824d27ff9254d
unknown <marcelobettini@yahoo.com.ar>
2022-04-03 17:06:32 -0300

    session

---
+++ middlewares/session.js
@@ -0,0 +1,21 @@
+const { tokenVerify } = require("../utils/handleJWT")
+
+const isAuth = async(req, res, next) => {
+    try {
+        if (!req.headers.authorization) {
+            return next(error)
+        }
+        const token = req.headers.authorization.split(" ").pop()
+        const validToken = await tokenVerify(token)
+        if (validToken instanceof Error) {
+            return next(error)
+        }
+        req.user = validToken
+        next()
+    } catch (error) {
+        error.status = 401
+        error.message = "Forbidden"
+        return next(error)
+    }
+}
+module.exports = isAuth
