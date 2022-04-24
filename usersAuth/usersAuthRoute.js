a08a8f7ae179ebf6601517d646ce374de6bc7822
unknown <marcelobettini@yahoo.com.ar>
2022-04-24 03:03:05 -0300

    ...

---
+++ usersAuth/usersAuthRoute.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const uploadFile = require("../utils/handleStorage");
+const { validatorCreateUser, validatorResetPassword } = require("../validators/users");
+const { register, login, forgot, reset, saveNewPass } = require("./usersAuthController")
+
+//register new users
+router.post("/register", uploadFile.single("file"), validatorCreateUser, register)
+//login
+router.post("/login", login)
+
+//TODO:
+//refresh token
+router.post("/refresh")
+
+//Forgot password
+router.post("/forgot-password", forgot)
+//Get Magic Link
+router.get("/reset/:token", reset);
+router.post("/reset/:token", validatorResetPassword, saveNewPass)
+module.exports = router
+
+
+
